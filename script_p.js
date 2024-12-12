56f99f28e74e53fa465eeed30bc5922d9f78248e
KATIA AG <a.katia2024@gmail.com>
2024-12-12 15:21:00 +0100

    Update script_p.js

--- script_p.js
+++ script_p.js
@@ -11,19 +11,11 @@ articleContainer.innerHTML = `<img src="${imgUrlsArr[0]}" class="image" />`;
 let imgIndex = 0;
 
 function previousImg() {
-  if (imgIndex > 0 && imgIndex < imgUrlsArr.length) {
-    imgIndex--;
-  } else {
-    imgIndex = imgUrlsArr.length - 1;
-  }
+  imgIndex = (imgIndex > 0) ? imgIndex - 1 : imgUrlsArr.length - 1;
   articleContainer.innerHTML = `<img src="${imgUrlsArr[imgIndex]}" class="image" />`;
 }
 
 function nextImg() {
-  if (imgIndex >= 0 && imgIndex < imgUrlsArr.length - 1) {
-    imgIndex++;
-  } else {
-    imgIndex = 0;
-  }
+  imgIndex = (imgIndex < imgUrlsArr.length - 1) ? imgIndex + 1 : 0;
   articleContainer.innerHTML = `<img src="${imgUrlsArr[imgIndex]}" class="image" />`;
 }
